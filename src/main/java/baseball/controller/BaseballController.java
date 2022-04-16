328a8b727f7987b56101722c9f0d5ca74e369227
gomtak <tak6831@gmail.com>
2022-04-16 16:47:06 +0900

    Comment: Add function list comment

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,39 @@
+package baseball.controller;
+
+public class BaseballController {
+
+//    임의의 1-9까지 서로다른 수 생성
+    public int makeRandomNumber(){
+        double a = Math.random();
+        System.out.println(a);
+        return 0;
+    }
+//    생성된 수 배열에 넣기.
+    public void putNumberToArray(){
+
+    }
+//    플레이어에게 1-9 서로다른 수 입력 받기.
+    public void inputNumberFromPlayer(){
+
+    }
+//    입력 값 검증
+    public void confirmNumber(){
+
+    }
+//    검증 후 결과 값 리턴
+    public void printScore(){
+
+    }
+//    정답 시 게임종료
+    public void gameSet(){
+
+    }
+//    에러 시 앱 종료
+    public void errorToPowerOff(){
+
+    }
+//    게임 종료시 게임 다시하기 및 앱 종료.
+    public void retryAndPowerOff(){
+
+    }
+}
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import baseball.controller.BaseballController;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -9,6 +11,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    BaseballController baseballController;
+//    임의의 1-9까지 서로다른 수 생성
+    @Test
+    void 랜덤한숫자만들기(){
+        baseballController.makeRandomNumber();
+    }
+//    생성된 수 배열에 넣기.
+//    플레이어에게 1-9 서로다른 수 입력 받기.
+//    입력 값 검증
+//    검증 후 결과 값 리턴
+//    정답 시 게임 종료
+//    에러 시 앱 종료
+//    게임 종료시 게임 다시하기 및 앱 종료.
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
