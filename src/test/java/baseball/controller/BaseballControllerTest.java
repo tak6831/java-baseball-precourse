6bf98980ff9f1570498b419097cb931644348440
gomtak <tak6831@gmail.com>
2022-04-17 16:16:49 +0900

    생성된 수 넣기

--- src/test/java/baseball/controller/BaseballControllerTest.java
+++ src/test/java/baseball/controller/BaseballControllerTest.java
@@ -1,5 +1,6 @@
 package baseball.controller;
 
+import baseball.entity.Ball;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,13 +8,22 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BaseballControllerTest {
-    BaseballController baseballController;
+    BaseballController baseballController = new BaseballController();
     //    임의의 1-9까지 서로다른 수 생성
     @Test
     void 랜덤한숫자만들기(){
-        assertThat(1).isEqualTo(baseballController.putNumberToArray(1,9));
+        assertThat(baseballController.makeRandomNumber())
+                .as("0보다 크고 10 보다 작은 정수")
+                .isGreaterThan(0)
+                .isLessThan(10);
     }
-//    생성된 수 배열에 넣기.
+//    생성된 수 볼배합에 넣기.
+    @Test
+    void 생성된수볼배합에넣기(){
+        Ball ball = baseballController.putNumberToEntity();
+        System.out.println(ball.toString());
+        assertThat(ball.isReady()).isTrue();
+    }
 //    플레이어에게 1-9 서로다른 수 입력 받기.
 //    입력 값 검증
 //    검증 후 결과 값 리턴
